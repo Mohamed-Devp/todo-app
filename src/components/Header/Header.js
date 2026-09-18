069bf10401df3a4229995d9fb138200ab2f7a56f
Mohamed Marghoub <mohamedmarghoub0726@gmail.com>
2026-09-18 12:05:09 +0100

    Implement the Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,51 @@
+import "./Header.css";
+
+import { createElement } from "../../utils.js";
+import createLogo from "../../components/Logo/Logo.js";
+
+import IconSun from "./icon-sun.svg";
+import IconMoon from "./icon-moon.svg";
+
+function createSwitch() {
+    const iconSunImg = createElement("img", {
+        className: "switch__icon switch__icon_theme_light",
+        attrs: {
+            src: IconSun,
+            alt: "",
+            title: "Switch to Light Theme",
+        },
+    });
+
+    const iconMoonImg = createElement("img", {
+        className: "switch__icon switch__icon_theme_dark",
+        attrs: {
+            src: IconMoon,
+            alt: "",
+            title: "Switch to Dark Theme",
+        },
+    });
+
+    const switchBtn = createElement("button", {
+        className: "switch switch_theme_light",
+        children: [iconSunImg, iconMoonImg],
+        attrs: {
+            role: "switch",
+            "aria-checked": "false",
+            "aria-label": "Toggle Dark Theme",
+        },
+    });
+
+    return switchBtn;
+}
+
+export default function createHeader() {
+    const logo = createLogo();
+    const switchBtn = createSwitch();
+
+    const header = createElement("header", {
+        className: "header",
+        children: [logo, switchBtn],
+    });
+
+    return header;
+}
