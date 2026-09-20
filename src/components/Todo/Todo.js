263bc7f3814b69fd0e11c0d94edfb0a21205247a
Mohamed Marghoub <mohamedmarghoub0726@gmail.com>
2026-09-20 14:45:15 +0000

    Implement the Todo component

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,42 @@
+import "./Todo.css";
+
+import todos from "../../todos.js";
+import { createElement } from "../../utils.js";
+import createCheckbox from "../Checkbox/Checkbox.js";
+
+import IconCross from "./icon-cross.svg";
+
+export default function createTodo(id, description, isCompleted) {
+    const checkboxId = `checkbox-${id}`;
+    const checkbox = createCheckbox(checkboxId, isCompleted, (e) => {
+        const isCompleted = e.target.checked;
+        todos.updateTodo(id, { isCompleted });
+    });
+
+    const descriptionLabel = createElement("label", {
+        className: "todo__description",
+        text: description,
+        attrs: { for: checkboxId },
+    });
+
+    const iconCrossImg = createElement("img", {
+        className: "todo__icon",
+        attrs: { src: IconCross, alt: "Remove", title: "Remove" },
+    });
+
+    const removeBtn = createElement("button", {
+        className: "todo__remove-btn",
+        children: [iconCrossImg],
+    });
+
+    removeBtn.addEventListener("click", () => {
+        todos.removeTodo(id);
+    });
+
+    const todo = createElement("li", {
+        className: "todo",
+        children: [checkbox, descriptionLabel, removeBtn],
+    });
+
+    return todo;
+}
