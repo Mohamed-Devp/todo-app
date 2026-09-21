34dfaaa4939a1c0b32c9ecfc3cc14908c8df6906
Mohamed Marghoub <mohamedmarghoub0726@gmail.com>
2026-09-21 15:30:52 +0000

    Implement handlers for todos:list-changed event

--- src/components/TodosPanel/TodosPanel.js
+++ src/components/TodosPanel/TodosPanel.js
@@ -1,6 +1,8 @@
 import "./TodosPanel.css";
 
+import pubsub from "../../pubsub.js";
 import { createElement } from "../../utils.js";
+import createTodo from "../../components/Todo/Todo.js";
 
 function createTabsBar() {
     const tabBtns = [
@@ -68,12 +70,28 @@ function createHeader() {
         children: [itemsLeft, tabs, clearBtn],
     });
 
+    pubsub.subscribe("todos:list-updated", (newList) => {
+        const activeTodos = newList.filter((todo) => !todo.isCompleted);
+        itemsLeft.textContent = `${activeTodos.length} items left`;
+    });
+
     return header;
 }
 
 function createList() {
     const list = createElement("ul", {
         className: "todos-panel__list",
+    });
+
+    pubsub.subscribe("todos:list-updated", (newList) => {
+        list.innerHTML = "";
+
+        newList.forEach((todo) => {
+            const { id, description, isCompleted } = todo;
+
+            const todoEl = createTodo(id, description, isCompleted);
+            list.appendChild(todoEl);
+        });
     });
 
     return list;
@@ -89,6 +107,14 @@ export default function createTodosPanel() {
     const todosPanel = createElement("div", {
         className: "todos-panel todos-panel_empty",
         children: [tabsBar, listContainer],
+    });
+
+    pubsub.subscribe("todos:list-updated", (newList) => {
+        if (newList.length > 0) {
+            todosPanel.classList.remove("todos-panel_empty");
+        } else {
+            todosPanel.classList.add("todos-panel_empty");
+        }
     });
 
     return todosPanel;
