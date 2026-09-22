3a297d0651b9f7424eb47c7a0c9efe925765a524
Mohamed Marghoub <mohamedmarghoub0726@gmail.com>
2026-09-22 12:05:53 +0000

    Build the index page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import "./global.css";
 import "./index.css";
+
+import createHeader from "./components/Header/Header.js";
+import createMain from "./components/Main/Main.js";
+
+document.body.append(createHeader(), createMain());
